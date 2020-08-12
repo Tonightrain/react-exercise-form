a413083713f0dcd16af2c584e1a797a3b4e0f2ef
MSI-PV <805560812@qq.com>
2020-08-12 21:50:47 +0800

    form

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,10 +1,89 @@
 import React, {Component} from 'react';
 import './myProfile.less';
+import './MyProfile.scss';
 
 class MyProfile extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      name:"",
+      gender:"Male",
+      description:"",
+      choose:false,
+    }
+  }
+  
+
+  handleNameChange = (event) =>{
+    this.setState({
+      name:event.target.value,
+    })
+  }
+
+  handleGenderChange = (event)=>{
+    this.setState({
+        gender:event.target.value,
+    })
+}
+handleDescriptionChange = (event)=>{
+  this.setState({
+    description:event.target.value,
+  })
+}
+
+handleChooseChange = (event)=>{
+  this.setState({
+    choose:event.target.value,
+  })
+}
+
+handleSubmit = (event) => {
+  alert(JSON.stringify(event));
+  event.preventDefault();
+}
+
   render() {
     return (
-      <form></form>
+      <form className="myForm" onSubmit={this.handleSubmit}>
+        <h1>My Profile</h1>
+        <div className="formName">
+          <label>Name</label><br></br>
+          <input type="text" value={this.state.name} onChange={this.handleNameChange}
+          placeholder="Your name" />
+        </div>
+        <div className="formGender">
+          <label>Gender</label><br></br>
+          <select onChange={this.handleGenderChange}>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+          </select>
+        </div>
+        <div className="formDescription">
+          <label>Description</label><br></br>
+          <input 
+            type="text"
+            value={this.state.description}
+            onChange={this.handleDescriptionChange}
+            placeholder="Description about yourself" />
+        </div>
+        <div className="formChoose">
+          <label>
+            <input type="checkbox" defaultChecked value={this.state.choose} onChange={this.handleChooseChange} />
+            I have read the terms of conduct
+          </label>
+        </div>
+        <div className="submit">
+          <button 
+          type="submit" 
+          value="Submit" 
+          style={{width:"160px",
+          height:"40px"}}
+          disabled={!this.state.name ||!this.state.gender ||!this.state.description ||!this.state.choose}
+          onClick={this.handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </form>
     );
   }
 }
